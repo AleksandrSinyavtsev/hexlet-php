5b017eabfe353f945c8aaa8b94df6ffee3da4529
Alex Sinyavtsev <sinyavtsev.alek@yandex.ru>
2023-05-01 12:11:24 +0300

    Conected autoload files

--- index.php
+++ index.php
@@ -1,1 +1,10 @@
-Hello, Hexlet!
+<?php
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+// Файл не включается напрямую
+// // Он загрузится автоматически благодаря автозагрузке
+use Hexlet\Php\Runner;
+
+print_r(Runner\run());
+
